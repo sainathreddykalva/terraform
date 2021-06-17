befb47d27e2da3cf951ad25b41f65370ea3cdff9
sainath <kalvasainathreddy@gmail.com>
2021-06-17 22:57:52 +0530

    sample.tf

--- 01-outputs/sample.tf
+++ 01-outputs/sample.tf
@@ -1,7 +1,13 @@
 output "sample" {
-  value = "hello world"
+//  value = "hello world"
+  value = var.sample
+
 }
 
 output "sample1" {
   value = "hellow world1"
+}
+
+variable "sample" {
+  default = "hellow world2"
 }
