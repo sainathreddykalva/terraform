26bac921fc426629148d9753d8fda143d43f362a
sainath <kalvasainathreddy@gmail.com>
2021-06-18 21:56:03 +0530

    sample.tf

--- 01-outputs/sample.tf
+++ 01-outputs/sample.tf
@@ -16,3 +16,55 @@ variable "sample" {
 ## var.sample used without quotes
 ## ${var.sample} used with in quotes
 ## Usually ${} used only when you combine with some other data
+## At any level single quotes does not  supported by terraform
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+variable "sample4" {
+  default =  [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+output "sample4" {
+  value = var.sample4[1]
+}
+
+variable "sample5" {
+  default =  {
+    string = "Hello",
+    number = 100,
+    boolean = true
+  }
+}
+
+output "sample5" {
+  value = var.sample5["boolean"]
+}
+
+//variable "sample6" {}
+//
+//output "sample6" {
+//  value = var.sample6
+//}
+//
+//variable "ENV" {}
+//
+//output "ENV" {
+//  value = var.ENV
+//}
