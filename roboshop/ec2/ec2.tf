7ef24cda10567622b1e0bf11d04e3d4690a76783
sainath <kalvasainathreddy@gmail.com>
2021-06-21 01:30:50 +0530

    var.COMPONENTS

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -16,14 +16,14 @@ resource "time_sleep" "wait" {
   depends_on          = [aws_spot_instance_request.cheap_worker]
   create_duration     = "120s"
 }
-//
-//resource "aws_ec2_tag" "spot" {
-//  count               = length(var.COMPONENTS)
-//  resource_id         = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-//  key                 = "Name"
-//  value               = element(var.COMPONENTS, count.index)
-//}
-//
+
+resource "aws_ec2_tag" "spot" {
+  count               = length(var.COMPONENTS)
+  resource_id         = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key                 = "Name"
+  value               = element(var.COMPONENTS, count.index)
+}
+
 //resource "aws_route53_record" "dns" {
 //  count               = length(var.COMPONENTS)
 //  zone_id             = "Z05564902CXVMD9UGH7W1"
