11bcd7ab23f21df1188090628fb2771fa15e3e1e
sainath <kalvasainathreddy@gmail.com>
2021-06-22 18:56:18 +0530

    var.COMPONENTS

--- roboshop/ec2.tf
+++ roboshop/ec2.tf
@@ -1,10 +1,10 @@
-module "frontend" {
+module "ec2" {
   source            = "./ec2"
   COMPONENTS        = var.COMPONENTS
 }
 
 module "ansible" {
-  depends_on        = [module.frontend]
+  depends_on        = [module.ec2]
   source            = "./ansible-apply"
   COMPONENTS        = var.COMPONENTS
 }
