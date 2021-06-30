af5a7bdc4de889e242d025e954263af7d459dcbe
sainath <kalvasainathreddy@gmail.com>
2021-06-30 23:54:01 +0530

    var.COMPONENTS

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "ansible-apply" {
-//  triggers = {
-//    abc = timestamp()
-//  }
+  //  triggers = {
+  //    abc = timestamp()
+  //  }
   count               = length(var.COMPONENTS)
   provisioner "remote-exec" {
 
@@ -16,7 +16,7 @@ resource "null_resource" "ansible-apply" {
       "sudo yum remove ansible -y",
       "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
       "sudo pip install ansible",
-      "ansible-pull -i localhost, -U https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps56/_git/ansible roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
+      "ansible-pull -i localhost, -U https://github.com/sainathreddykalva/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
     ]
 
   }
