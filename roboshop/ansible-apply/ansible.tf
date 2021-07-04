c631a563d4a761fd35eba62a24d3d239f310d8e3
sainath <kalvasainathreddy@gmail.com>
2021-07-04 17:18:19 +0530

    install python 20.3.4 version

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -15,6 +15,10 @@ resource "null_resource" "ansible-apply" {
       "sudo yum install ansible -y",
       "sudo yum remove ansible -y",
       "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
+      "sudo yum remove python-pip",
+      "sudo cd /usr/local/src",
+      "sudo wget https://bootstrap.pypa.io/pip/2.7/get-pip.py",
+      "sudo python get-pip.py",
       "sudo pip install ansible==4.1.0",
       "ansible-pull -i localhost, -U https://github.com/sainathreddykalva/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
     ]
