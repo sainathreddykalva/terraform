623ea1ed66c289a94e028dc27592238dced0e3a3
sainath <kalvasainathreddy@gmail.com>
2021-07-06 19:14:43 +0530

    down grade to ansible 3.4.0 because bugs in 4.1.0

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -15,11 +15,11 @@ resource "null_resource" "ansible-apply" {
       "sudo yum install ansible -y",
       "sudo yum remove ansible -y",
       "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
-      "sudo yum remove python-pip -y",
-      "sudo cd /usr/local/src",
-      "sudo wget https://bootstrap.pypa.io/pip/2.7/get-pip.py",
-      "sudo python get-pip.py",
-      "sudo pip install ansible==4.1.0",
+//      "sudo yum remove python-pip -y",
+//      "sudo cd /usr/local/src",
+//      "sudo wget https://bootstrap.pypa.io/pip/2.7/get-pip.py",
+//      "sudo python get-pip.py",
+      "sudo pip install ansible==3.4.0",
       "ansible-pull -i localhost, -U https://github.com/sainathreddykalva/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
     ]
 
