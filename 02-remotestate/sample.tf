eba519ee01ddd3f1af0c67810f8071cd53b5649a
sainath <kalvasainathreddy@gmail.com>
2021-07-06 22:22:24 +0530

    vpc creation for dev and prod

---
+++ 02-remotestate/sample.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "sample" {
+  ami                     = "ami-059e6ca6474628ef0"
+  instance_type           = "t2.micro"
+}
+
+terraform {
+  backend "s3" {
+    bucket                = "terraform-practice56"
+    key                   = "sample/terraform.tfstate"
+    region                = "us-east-1"
+    dynamodb_table        = "terraform"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
