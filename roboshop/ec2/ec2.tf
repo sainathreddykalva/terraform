66210a64a6fb01d3f5a82cb02f1ff26ea7e81d58
sainath <kalvasainathreddy@gmail.com>
2021-07-12 19:03:57 +0530

    added wait_for_fulfillment=true for spot instance

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -4,6 +4,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   spot_price          = "0.0031"
   instance_type       = "t3.micro"
   vpc_security_group_ids = ["sg-09dfd90ba829f4166"]
+  wait_for_fulfillment = true
 
   tags                = {
     Name              = element(var.COMPONENTS, count.index)
